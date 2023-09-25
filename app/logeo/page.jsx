2a86b8a517a1aa08b832cb06c7eafc3716f0a6c6
1109car <123415154+1109car@users.noreply.github.com>
2023-09-25 00:59:20 -0500

    .........

--- app/logeo/page.jsx
+++ app/logeo/page.jsx
@@ -7,33 +7,17 @@ import  '../page.module.css'
 export default function page() {
     
   return (
-
-    <div className="contenedor boton-clic">
-     
-
-        <div><h1 className='bienvenido'>Bienvenido!</h1></div>
-        <form className='form'>
-            <label className='nombre'>Email</label>
-           <input type="text"  placeholder='Email' name='email' className='input'/>
-         <label   className='nombre dos'>Contraseña</label>
-         <input type="text" placeholder='Contraseña' name='contraseña' className='tres'/>
-         <input type='input' className='sino boton-clic'
-          placeholder='si aun no te reuiat'/>
-         <div className='registrate' id='registrate'>
-          
-          <label className='name'>Nombre</label>
-          <input type="text" className='in a'/>
-          <label className='apellido'>Apellido</label>
-          <input type="text" className='in b'/>
-          <label className='emas'>Email</label>
-          <input type="text" className='in c'/>
-          <label className='contr'>Contraseña</label>
-          <input type="text" className='in d' />
-         </div>
-         <input type="submit" className='enviar' value="Enviar"/>
-         {/* <input type="hidden" name='_next' value={"http://localhost:3000/"} /> */}
-        </form>
-    <img className='imagen' src={("https://www.bbva.pe/content/dam/public-web/peru/photos/blog-img/card/card-vehiculos-seguros.jpg")} alt="" />
-    </div>
+    <div className="contenedor">
+    <div><h1 className='bienvenido'>Bienvenido!</h1></div>
+    <form className='form'>
+        <label className='nombre'>Email</label>
+       <input type="text"  placeholder='Email' name='email' className='input'/>
+     <input type="text" placeholder='Contraseña' name='contraseña' className='tres'/>
+        <Click></Click>
+     <input type="submit" className='enviar' value="Enviar"/>
+     {/* <input type="hidden" name='_next' value={"http://localhost:3000/"} /> */}
+    </form>
+<img className='imagen' src={("https://www.bbva.pe/content/dam/public-web/peru/photos/blog-img/card/card-vehiculos-seguros.jpg")} alt="" />
+</div>
   )
 }
